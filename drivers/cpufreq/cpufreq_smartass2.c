e516b857aa6ce19b10595afd171ae1f0c0ee4b1e
fuzz <fuzzzzzzzzzzzzzzzzzz@gmail.com>
2012-10-23 15:51:56 +0900

    cpufreq: Fix build

--- drivers/cpufreq/cpufreq_smartass2.c
+++ drivers/cpufreq/cpufreq_smartass2.c
@@ -836,11 +836,8 @@ static int __init cpufreq_smartass_init(void)
 	}
 
 	// Scale up is high priority
-	// FIXME
-	// up_wq = alloc_workqueue("ksmartass_up", WQ_HIGHPRI, 1);
-	// down_wq = alloc_workqueue("ksmartass_down", 0, 1);
-	up_wq = create_rt_workqueue("ksmartass_up");
-	down_wq = create_workqueue("ksmartass_down");
+	up_wq = alloc_workqueue("ksmartass_up", WQ_HIGHPRI, 1);
+	down_wq = alloc_workqueue("ksmartass_down", 0, 1);
 	if (!up_wq || !down_wq)
 		return -ENOMEM;
 
